6dcdb3a55775f36d90519950bdb00d7676cb6eea
CapsDoTi <capsdoti@gmail.com>
2026-06-09 09:51:34 -0300

    feat: salvar mais dados

--- app/src/main/java/com/example/myshare/MainActivity.kt
+++ app/src/main/java/com/example/myshare/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.core.view.WindowInsetsCompat
 
 const val PREFS_NAME = "meus dados"
 const val KEY_NOME = "nome"
+const val  KEY_EMAIL = "email"
+const val  KEY_TELEFONE = "telefone"
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,22 +27,28 @@ class MainActivity : AppCompatActivity() {
         }
 
         val txtEdit = findViewById<TextView>(R.id.id_nome)
+        val txtEditEmail = findViewById<TextView>(R.id.id_email)
+        val txtEditTel = findViewById<TextView>(R.id.id_telefone)
         val txtResultado = findViewById<TextView>(R.id.id_resultado)
         val btnCarregar = findViewById<Button>(R.id.id_btn_carregar)
         val btnSalvar = findViewById<Button>(R.id.id_btn_salvar)
 
         btnSalvar.setOnClickListener {
             val nome = txtEdit.text.toString()
+            val email = txtEditEmail.text.toString()
+            val telefone = txtEditTel.text.toString()
 
             val prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE)
             val editor = prefs.edit()
 
             editor.putString(KEY_NOME, nome)
+                .putString(KEY_EMAIL, email)
+                .putString(KEY_TELEFONE, telefone)
             editor.apply()
 
             Toast.makeText(
                 this@MainActivity,
-                "Nome salvo com sucesso",
+                "Dados salvos com sucesso",
                 Toast.LENGTH_SHORT
             ).show()
         }
@@ -48,7 +56,9 @@ class MainActivity : AppCompatActivity() {
         btnCarregar.setOnClickListener {
             val prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE)
             val nome = prefs.getString(KEY_NOME, "Nenhum nome salvo")
-            txtResultado.text = "Nome: $nome"
+            val email = prefs.getString(KEY_EMAIL, "Nenhum nome salvo")
+            val telefone = prefs.getString(KEY_TELEFONE, "Nenhum nome salvo")
+            txtResultado.text = "Nome: $nome\n Email: $email\n Telefone: $telefone"
         }
     }
 }
